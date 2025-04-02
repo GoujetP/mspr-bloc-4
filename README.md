c0e0f940f470f4052a23ae9ce446489b08b9afb6
GoujetP <pierregoujet29@gmail.com>
2025-04-02 20:35:58 +0200

    init customer

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mspr-bloc-4
+
+## Customers
+
+```
+npm init -y
+
+npm install express
+
+npm install --save-dev typescript @types/node @types/express ts-node
+```
---
+++ customers/src/types/customer.interface.ts
@@ -0,0 +1,12 @@
+export interface Customer {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    postalCode: string;
+    city: string;
+    company: string;
+}
